2f527a84347876f988500c204cf460ce66bfa71d
Etens <45323562+Etens@users.noreply.github.com>
2023-01-25 16:48:14 +0200

    NavBar Done

--- link-up/src/components/NavBar.jsx
+++ link-up/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCog, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import {faUser, faSignOutAlt, faBell} from "@fortawesome/free-solid-svg-icons";
 import logoLink from "../logo-link.png";
 
 import "./NavBar.css";
@@ -9,12 +9,22 @@ export default function NavBar() {
   return (
     <div>
       <nav className="navbar-container">
-        <img className="navbar-logo" src={logoLink} alt="Link-logo" onClick={() => window.location.href = "/"} />
+        <img
+          className="navbar-logo"
+          src={logoLink}
+          alt="Link-logo"
+          onClick={() => (window.location.href = "/")}
+        />
         <button className="navbar-button">
-          <FontAwesomeIcon icon={faCog} title="Settings" />
+          <FontAwesomeIcon icon={faUser} title="Settings" 
+            onClick={() => (window.location.href = "/settings")} />
         </button>
         <button className="navbar-button">
           <FontAwesomeIcon icon={faSignOutAlt} title="Logout" />
+        </button>
+        <button className="navbar-button">
+          <FontAwesomeIcon icon={faBell} title="Notifications" />
+          <span className="notification-count">3</span>
         </button>
       </nav>
     </div>
