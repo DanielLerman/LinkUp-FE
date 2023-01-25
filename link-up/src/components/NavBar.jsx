ea8ee0c7295a16e376125bad7f83a6a1c2aacf79
Etens <45323562+Etens@users.noreply.github.com>
2023-01-25 12:40:11 +0200

    initial commit

---
+++ link-up/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function NavBar() {
+  return (
+    <div>
+        {/* i want create a navbar with two buttons for setting and logout use icons from fontawesome */}
+
+    </div>
+  )
+}
