9ca486f93201d55ab7c2194ea38484cce8946681
Etens <45323562+Etens@users.noreply.github.com>
2023-01-26 11:55:02 +0200

    not use

--- link-up/src/App.js
+++ link-up/src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import linkUpContext from "./context/context";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
-import WelcomePage from "./pages/WelcomePage";
+import WelcomePage from "./pages/welcomePage";
 import index from './index.css'
 import Settings from "./pages/Settings";
 // import ModalLoginSign from "./pages/ModalLoginSign";
@@ -14,6 +14,7 @@ function App() {
     <BrowserRouter>
      <NavBar/>
      <Routes>
+      <Route path="/settings" element={<Settings/>}/>
       <Route path="/" element={<WelcomePage/>}/>
      <Route path="/home" element={<HomePage />} /> 
      </Routes>
